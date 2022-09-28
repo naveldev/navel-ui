7c95692c6e30e9d982a150351878c1c0b55fef83
Navi Ismail <ismailnavarone@gmail.com>
2022-09-28 09:41:16 +0200

    :bug: Fixed modal styling

--- packages/vuejs-components/src/js/components/UI/VModal.vue
+++ packages/vuejs-components/src/js/components/UI/VModal.vue
@@ -1,33 +1,35 @@
 <template>
     <div class="modal p-5" :class="{ 'show': isActive }" @click.prevent="triggerClose">
-        <transition name="animation-modalAnimation" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut" mode="out-in" appear>
-            <div class="modal-container" v-if="isActive">
-                <slot name="modal">
-                    <!-- Title -->
-                    <slot name="modal-header">
-                        <div class="modal-heading d-flex">
-                            <slot name="modal-title" />
+        <div>
+            <transition name="animation-modalAnimation" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut" mode="out-in" appear>
+                <div class="modal-container" v-if="isActive">
+                    <slot name="modal">
+                        <!-- Title -->
+                        <slot name="modal-header">
+                            <div class="modal-heading d-flex">
+                                <slot name="modal-title" />
 
-                            <div class="ml-auto color-muted p-1">
-                                <a @click.prevent="this.setState(false)">
-                                    <v-icon icon="fa-xmark-circle" />
-                                </a>
+                                <div class="ml-auto color-muted p-1">
+                                    <a @click.prevent="this.setState(false)">
+                                        <v-icon icon="fa-xmark-circle" />
+                                    </a>
+                                </div>
                             </div>
+                        </slot>
+
+                        <!-- Body -->
+                        <div v-if="this.$slots['modal-content']" class="modal-content">
+                            <slot name="modal-content" />
+                        </div>
+
+                        <!-- Footer -->
+                        <div v-if="this.$slots['modal-footer']" class="modal-footer">
+                            <slot name="modal-footer" />
                         </div>
                     </slot>
-
-                    <!-- Body -->
-                    <div v-if="this.$slots['modal-content']" class="modal-content">
-                        <slot name="modal-content" />
-                    </div>
-
-                    <!-- Footer -->
-                    <div v-if="this.$slots['modal-footer']" class="modal-footer">
-                        <slot name="modal-footer" />
-                    </div>
-                </slot>
-            </div>
-        </transition>
+                </div>
+            </transition>
+        </div>
     </div>
 </template>
 
