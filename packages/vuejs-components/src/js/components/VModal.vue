f3ec7cfe71d818e8494a9ee8709f8260cd1f78c0
Navi Ismail <ismailnavarone@gmail.com>
2022-10-06 16:12:36 +0200

    :bug: Correctly centered modal

--- packages/vuejs-components/src/js/components/VModal.vue
+++ packages/vuejs-components/src/js/components/VModal.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="modal p-5" :class="{ 'show': isActive }" @click.prevent="triggerClose">
-        <div class="d-flex justify-content-center">
+        <div>
             <transition name="animation-modalAnimation" enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut" mode="out-in" appear>
                 <div class="modal-container" v-if="isActive">
                     <slot name="modal">
