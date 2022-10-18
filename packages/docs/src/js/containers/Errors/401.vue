87baadaf86f1d25784c998e3f6a9b22aec38e50e
Navi Ismail <nismail@fletcher.nl>
2022-10-18 10:44:56 +0200

    :new: Added error pages

---
+++ packages/docs/src/js/containers/Errors/401.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="container py-5">
+		<div class="row">
+			<div class="col-md-3"></div>
+			<div class="col-xs-12 col-md-6">
+                Unauthorized!
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+	export default {
+		layout: 'Stylesheet',
+	}
+</script>
--- packages/docs/src/js/routes/default.js
+++ packages/docs/src/js/routes/default.js
@@ -5,5 +5,17 @@ export default [
 		path: '/',
 		name: 'home',
 		component: Container('Home')
-	}
+	},
+
+	{
+		path: '/unauthorized',
+		name: '401',
+		component: Container('Errors/401')
+	},
+
+	{
+		path: '/:slug',
+		name: '404',
+		component: Container('Errors/404')
+	},
 ]
